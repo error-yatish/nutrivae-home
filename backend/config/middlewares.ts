f17c5713d48307cb736329130c748247c250401b
Yatish Dhanani <dhananiyatish@gmail.com>
2026-07-19 20:34:53 +0530

    Added CORS.

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -7,7 +7,15 @@ const config: Core.Config.Middlewares = [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:4321', 'http://127.0.0.1:4321', 'http://localhost:3000'],
+      origin: [
+        'http://localhost:4321',
+        'http://127.0.0.1:4321',
+        'http://localhost:3000',
+        'https://nutrivae-backend.onrender.com',
+        'https://nutrivae-frontend.onrender.com',
+        'https://nutrivae.onrender.com',
+        '*'
+      ],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       keepHeaderOnError: true,
